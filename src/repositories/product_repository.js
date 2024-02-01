3e14ed2d82fa1810bddd5d2448e1e1f03712ed49
arunrawat404 <arunrawatar404@gmail.com>
2024-02-01 20:23:58 +0530

    Added crud,product repository

---
+++ src/repositories/product_repository.js
@@ -0,0 +1,10 @@
+const { Product } = require("../models/product");
+const CrudRepository = require("./crud_repository");
+
+class ProductRepository extends CrudRepository {
+    constructor() {
+        super(Product)
+    };
+};
+
+module.exports = ProductRepository;
