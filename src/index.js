2647b6b3d252faf740572f2b698e98010c2a198f
arunrawat404 <arunrawatar404@gmail.com>
2024-02-01 19:44:12 +0530

    Added db to server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { PORT } = require("./config/server_config");
+const connect = require("./config/db_config.js");
 
 const app = express();
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is up and running on PORT ${PORT}`);
-})
+    await connect();
+    console.log("DB connected");
+}) 
