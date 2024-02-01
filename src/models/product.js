320960873d6682c61fa03ea456d44452fbe7c310
arunrawat404 <arunrawatar404@gmail.com>
2024-02-01 20:09:22 +0530

    Added product and variant models

---
+++ src/models/product.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const variantSchema = new mongoose.Schema({
+    variantName: {
+        type: String,
+        required: true
+    },
+    variantName: {
+        type: String,
+        required: true
+    },
+    SKU: {
+        type: String,
+    },
+    stockCount: {
+        type: Number,
+        required: true
+    }
+});
+
+const Variant = mongoose.model('Variant', variantSchema);
+
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    variants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Variant"
+    }]
+})
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = {
+    Product, Variant
+}
